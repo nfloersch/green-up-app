f4f0d3cbf1508610da9970a910ca858f815b404d
Tyler O <ortizt89@gmail.com>
2024-03-24 14:53:33 -0400

    Switch to more generic environment switches on eas.json to then load into app config.

--- app.config.js
+++ app.config.js
@@ -1,8 +1,20 @@
+// To assist with local dev setup, we load .env configs
 require('dotenv').config();
-const firebaseConfig = require(process.env.FIREBASE_CONFIG_FILE)
 
 // https://docs.expo.dev/workflow/configuration/#configuration-resolution-rules
 export default ({ config }) => {
+    let firebaseTarget = ''
+    // ENVIRONMENT is set from eas.json to set the build environment context
+    if (process.env.ENVIRONMENT === 'dev' || process.env.ENVIRONMENT === 'local') {
+        firebaseTarget = process.env.FIREBASE_CONFIG_DEV || ''
+    } else if (process.env.ENVIRONMENT === 'qa') {
+        firebaseTarget = process.env.FIREBASE_CONFIG_QA || ''
+    } else if (process.env.ENVIRONMENT === 'prod') {
+        firebaseTarget = process.env.FIREBASE_CONFIG_PROD || ''
+    }
+
+    const firebaseConfig = require(firebaseTarget)
+
     const newConfig = {
         ...config,
         extra: {
